54b89829c118fe5e4204ed7c9c587a87f1030b2f
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-01-29 12:35:10 +0530

    javascript lecture

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var myUserData = 12345;
+console.log(myUserData);
+
+var data = 12;
+console.log("data :",data);
+console.log("data type:",typeof(data));
+
+var data1 = '12';
+console.log("data :",
+
+data1);
+console.log("data type:",typeof(data1));
+
+var data2 = false;
+console.log("data :",data2);
+console.log("data type:",typeof(data2));
