3c5fe78560b35330cddd870c03906b9116267318
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-09 22:10:17 +0530

    updates

---
+++ day18.javascript.js
@@ -0,0 +1,38 @@
+var number1 = 20;
+var number2 = 30;
+
+if(number1 > number2){
+   console.log("number1 is greater than number2")
+}else{
+    console.log("number2 is greater than number1")
+}
+
+
+// Q - check is user applicable for driving licence or not?
+// if -else statemet
+var UserAge = 20;
+if(UserAge > 18){
+    console.log("The user is applicable for licence")
+}else{
+    console.log("The user is not applicable for licence")
+}
+
+
+// If-elseif-else statement
+
+var Userage=18;
+if(Userage >18){
+console.log("The user is applicable for licence")
+}
+else if(Userage == 18){
+    console.log("The user is applicable for  learning licence")
+}
+else{
+    console.log("The user is not applicable for licence")
+}
+
+// Modulus
+   
+
+
+  
--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
-var myUserData = 12345;
-console.log(myUserData);
+// var myUserData = 12345;
+// console.log(myUserData);
 
-var data = 12;
-console.log("data :",data);
-console.log("data type:",typeof(data));
+// var data = 12;
+// console.log("data :",data);
+// console.log("data type:",typeof(data));
 
-var data1 = '12';
-console.log("data :",
+// var data1 = '12';
+// console.log("data :",
 
-data1);
-console.log("data type:",typeof(data1));
+// data1);
+// console.log("data type:",typeof(data1));
 
-var data2 = false;
-console.log("data :",data2);
-console.log("data type:",typeof(data2));
+// var data2 = false;
+// console.log("data :",data2);
+// console.log("data type:",typeof(data2));
+
+console.log
+ 
+
+
