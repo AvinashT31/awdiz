ab4235dd99250a096ece1279c92abc5e30bad04b
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-20 14:46:32 +0530

    update

--- js/day22.js
+++ js/day22.js
@@ -41,4 +41,27 @@ function addTodo(){
     }
 }
 
+function cs(){
+    var mainDiv = document.getElementById("displayTodos");
+    var div = document.createElement("div");
+    var h1 = document.createElement("h1");
+    h1.innerText = "structure created !";
+    h1.style.color = "blue";
+    div.append(h1);
+    var h1sec = document.createElement("h1");
+    h1sec.innerText = "hello world";
+    h1sec.style.color = "red";
+    div.append(h1sec);
+    mainDiv.append(div);
+}
+
  
+// function cs(){
+//     var mainDiv = document.getElementById("displayTodos");
+//     var div = document.createElement("div");
+//     var h1 = document.createElement("h1");
+//     h1.innerText = "hello world";
+//     div.append(h1);
+//     mainDiv.append(div);
+    
+// }
