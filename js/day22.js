9553f0db8f8f39c32fcdc33d91c8805773366168
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-21 12:21:07 +0530

    update

--- js/day22.js
+++ js/day22.js
@@ -30,10 +30,11 @@ function addTodo(){
     if(gettingTodos){
         console.log(gettingTodos,"gettingTodos");
         var myTodosFromLS = JSON.parse(localStorage.getItem("Todos")) || [];
-        myTodosFromLS.push(gettingTodos);8
+        myTodosFromLS.push(gettingTodos);
         // console.log(myTodosFromLS,"myTodosFromLS");
         localStorage.setItem("Todos", JSON.stringify(myTodosFromLS));
         document.getElementById('todoHere').value = "";
+        cs(); // (whenever we click on submit todo the  updated todo list will display)
         alert("Todo added !");
     }
     else{
@@ -42,20 +43,51 @@ function addTodo(){
 }
 
 function cs(){
-    var mainDiv = document.getElementById("displayTodos");
-    var div = document.createElement("div");
-    var h1 = document.createElement("h1");
-    h1.innerText = "structure created !";
-    h1.style.color = "blue";
-    div.append(h1);
-    var h1sec = document.createElement("h1");
-    h1sec.innerText = "hello world";
-    h1sec.style.color = "red";
-    div.append(h1sec);
-    mainDiv.append(div);
-}
+     
+    var divFromhtml = document.getElementById("displayTodos")
 
- 
+     var gettingTodosFromLS = JSON.parse(localStorage.getItem("Todos"));
+     console.log(gettingTodosFromLS, "gettingTodosFromLS");
+
+     var finalstructure = [`<div id="row">
+     <p>task</p> <p>completed</p>
+     </div>`]
+
+     for(var i=0; i< gettingTodosFromLS.length; i++ ){
+        // console.log(gettingTodosFromLS[i])
+        // var div= document.createElement("div");
+        // var p= document.createElement("p");
+        // p.innerText= gettingTodosFromLS[i];
+        // div.append(p);
+        // finalstructure.push(div);
+
+        finalstructure += `<div id= "content">
+        <p>${gettingTodosFromLS[i]}</p>
+        <i onclick = "deleteTodo(${i})" class="fa-solid fa-trash"></i>
+        </div>`
+ }
+
+  divFromhtml.innerHTML = finalstructure;
+
+//   console.log(finalstructure, "finalstructure")
+
+
+    // var mainDiv = document.getElementById("displayTodos");
+    // var div = document.createElement("div");
+    // var h1 = document.createElement("h1");
+    // h1.innerText = "structure created !";
+    // h1.style.color = "blue";
+    // div.append(h1);
+    // var h1sec = document.createElement("h1");
+    // h1sec.innerText = "hello world";
+    // h1sec.style.color = "red";
+    // div.append(h1sec);
+    // mainDiv.append(div);
+}
+cs() // (to automatic display)
+
+// <button onclick="cs()">creating structure</button>
+
 // function cs(){
 //     var mainDiv = document.getElementById("displayTodos");
 //     var div = document.createElement("div");
@@ -65,3 +97,11 @@ function cs(){
 //     mainDiv.append(div);
     
 // }
+
+
+function deleteTodo(todoIndex){
+    var todosFromLS = JSON.parse(localStorage.getItem("Todos"));
+    todosFromLS.splice(todoIndex, 1)
+    localStorage.setItem("Todos", JSON.stringify(todosFromLS));
+    cs(); 
+}
