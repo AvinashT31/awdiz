ad83e9296103bfc7897bfb9d95eb66c459472112
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-26 12:36:38 +0530

    update

--- js/Q & A.js
+++ js/Q & A.js
@@ -19,16 +19,48 @@
 
 // Q.print count number from 147 to 254, ehich is divisible by 3 and odd
 
-for(var i=147; i<=254; i++){
-    if(i%3===0 && i%2!==0){
-        console.log(i);
+// for(var i=147; i<=254; i++){
+//     if(i%3===0 && i%2!==0){
+//         console.log(i);
+//     }
+// }
+
+// var count=0
+// for(var i=147; i<=254; i++){
+//     if(i%3===0 && i%2!==0){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+// Q.10 find given number is present in given array or not, if found return true else false
+
+var array = [1,2,443,454,232,546,23,567,676,2342,565,4,2,6]
+var number = 6;
+
+// Number1 = array.includes(number);
+// console.log(Number1, "Number1");
+
+// if(array.includes(number)){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+var falg = false;
+
+
+for (var i = 0; i < array.length; i++) {
+    if (array [i] == number){
+        // console.log(number, "number")
+        falg = true;
     }
 }
 
-var count=0
-for(var i=147; i<=254; i++){
-    if(i%3===0 && i%2!==0){
-        count++;
-    }
+if (falg) {
+    console.log("true")
+} else {
+    console.log("false");
 }
-console.log(count);
